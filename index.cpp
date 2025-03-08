c3672c21f85edbf91bc89c817badb833a1f1f4ea
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2025-03-08 09:43:49 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,count=0;
+    cout<<"Enter the number to check prime or not: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            count++;
+        }
+    }
+   if (count==2)
+   {
+    cout<<n<<" "<<"is a prime number";
+   }else{
+    cout<<n<<" "<<"is not a prime number";
+   }
+
+   
+}
